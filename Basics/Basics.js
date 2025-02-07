9048528ec996fc220bf53c4e5e8905a6cdee6531
Bhaskar Guha <95684532+BhaskarGuha@users.noreply.github.com>
2025-02-07 14:36:27 +0000

    Basics

--- Basics/Basics.js
+++ Basics/Basics.js
@@ -1,1 +1,21 @@
-console.log("Bhaskar")
+const rollNumber = 150
+let email = "abc@gmail.com"
+var password = "1234"
+
+console.log(rollNumber);
+
+//rollNumber = 125
+
+console.table([rollNumber, email, password]) // Assignment to constant variable. Not possible to change the value.
+
+/* console.table([]) this statetment use to print the all values in table format with indexes ex 
+
+┌─────────┬─────────────────┐
+│ (index) │ Values          │
+├─────────┼─────────────────┤
+│ 0       │ 150             │
+│ 1       │ 'abc@gmail.com' │
+│ 2       │ '1234'          │
+└─────────┴─────────────────┘
+
+*/
